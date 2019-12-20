da1941dc97600863c78d454e3cf0f0fccbcd25a0
crismagu <cmg1205@gmail.com>
2019-12-20 11:12:56 -0500

    redid in react, trying to keep this all in one place

---
+++ index.js
@@ -0,0 +1,17 @@
+// import React from "react"
+// import "./app.css"
+
+// class App extends React.Component {
+//     constructor(){
+//         this.state = {
+
+//         } //the current state of being in the app, the properties in the application
+//     }
+//     render() {
+//         return (
+//             <div className= "App">
+//                 <title text
+//             </div>
+//         )
+//     }
+// }
